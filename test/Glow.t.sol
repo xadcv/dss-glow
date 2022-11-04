1f1a0552e26334667238d42fc5093c528809259c
cambyses21 <adcv@hey.com>
2022-11-04 21:42:48 +0100

    Adding GUSD tokens from the faucet works

--- test/Glow.t.sol
+++ test/Glow.t.sol
@@ -4,26 +4,31 @@ pragma solidity ^0.8.13;
 import "forge-std/Test.sol";
 import "../src/Glow.sol";
 
-interface ChainLogLike {
-    function getAddress(bytes32) external view returns (address);
+interface RestrictedTokenFaucet {
+    function gulp(address gem, address[] calldata addrs) external;
 }
 
 contract GlowTest is Test {
     ChainLogLike constant changelog =
         ChainLogLike(0xdA0Ab1e0017DEbCd72Be8599041a2aa3bA7e740F);
-    Glow public glow;
-    uint256 goerliFork;
+    RestrictedTokenFaucet constant rtf =
+        RestrictedTokenFaucet(0xa473CdDD6E4FAc72481dc36f39A409D86980D187);
 
+    address[] testers = [address(this)];
+
+    Glow public glow;
+
+    uint256 goerliFork;
     string GOERLI_RPC_URL = vm.envString("GOERLI_RPC_URL");
 
     function setUp() public {
         goerliFork = vm.createFork(GOERLI_RPC_URL);
-
-        Glow glow = new Glow();
+        glow = new Glow(changelog.getAddress("CHANGELOG"));
+        rtf.gulp(changelog.getAddress("GUSD"), testers);
     }
 
-    function testGusdAddress() public {
-        address gusd = changelog.getAddress("GUSD");
-        assertEq(gusd, 0x67aeF79654D8F6CF44FdC08949c308a4F6b3c45B);
+    function testGusdBalance() public {
+        Gusd gusd = Gusd(changelog.getAddress("GUSD"));
+        assertEq(gusd.balanceOf(address(this)), 50_000_00);
     }
 }
