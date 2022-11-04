ca92e05b305a79ea54018d35a136fc0c2aaa653d
cambyses21 <adcv@hey.com>
2022-11-04 21:42:48 +0100

    Adding GUSD tokens from the faucet works

--- src/Glow.sol
+++ src/Glow.sol
@@ -1,14 +1,47 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
+interface Gusd {
+    function balanceOf(address _owner) external view returns (uint256);
+
+    function approve(address _addr, uint256 _amt) external returns (bool);
+
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    ) external returns (bool);
+}
+
+interface Dai {
+    function balanceOf(address) external returns (uint256);
+
+    function approve(address usr, uint256 wad) external returns (bool);
+
+    function transferFrom(
+        address src,
+        address dst,
+        uint256 wad
+    ) external returns (bool);
+}
+
+interface ChainLogLike {
+    function getAddress(bytes32) external view returns (address);
+}
+
 contract Glow {
-    uint256 public number;
+    ChainLogLike public immutable changelog;
+    Gusd public immutable gusd;
+    Dai public immutable dai;
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
-    }
+    address gusdAddress;
+    address daiAddress;
 
-    function increment() public {
-        number++;
+    constructor(address chainlog_) {
+        changelog = ChainLogLike(chainlog_);
+        gusdAddress = changelog.getAddress("GUSD");
+        daiAddress = changelog.getAddress("MCD_DAI");
+        gusd = Gusd(gusdAddress);
+        dai = Dai(daiAddress);
     }
 }
